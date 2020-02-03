ec8b1ade6b21e71f3e67f5eea94500b695b7070f
DiogoBotton <diogo.luo@gmail.com>
2020-02-03 17:44:23 -0300

    exercicios

--- SPRINT 1 - BD/Scripts SQL (códigos DDL, DML e SQL/SPRINT1-2DEV-OPTUS-DiogoBotton.sql
+++ SPRINT 1 - BD/Scripts SQL (códigos DDL, DML e SQL/SPRINT1-2DEV-OPTUS-DiogoBotton.sql
@@ -4,7 +4,7 @@ select Nome, DataLancamento from Albuns where ArtistaId = 1;
 --Selecionar albuns Lançados na mesma dataasd
 select Nome, DataLancamento from Albuns where DataLancamento like '%1992-03-27%';
 --ou
-select A.Nome, AR.Nome from Albuns as A join Artistas as AR on a.EstiloId = ar.Id; 
+select A.Nome, AR.Nome from Albuns as A join Artistas as AR on a.ArtistaId = ar.Id; 
 
 --Selecionar Artistas de mesmo estilo musical
 create procedure GetArtistaPorEstilo
@@ -15,4 +15,4 @@ select * from Artistas where EstiloId = @EstiloId;
 execute GetArtistaPorEstilo 2;
 
 --Selecionar album e artistas, data mais recente para o mais antigo.
-select Nome, DataLancamento from Albuns order by DataLancamento desc;
+select A.Nome, AR.Nome, A.DataLancamento from Albuns as A join Artistas as AR on a.ArtistaId = ar.Id order by DataLancamento desc;
